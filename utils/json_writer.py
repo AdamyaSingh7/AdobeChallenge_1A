df6abd2c7b466cb9aa121a2c3e68c8778e93c40c
AdamyaSingh7 <adamyasingh54@gmail.com>
2025-07-28 14:30:11 +0530

    Update: deleted unused folders

--- utils/json_writer.py
+++
@@ -1,5 +0,0 @@
-import orjson
-
-def write_json(data, output_path):
-    with open(output_path, "wb") as f:
-        f.write(orjson.dumps(data, option=orjson.OPT_INDENT_2))
--- utils/layout_utils.py
+++
@@ -1,3 +0,0 @@
-def extract_text_features(text_block, page_height):
-    # Implement advanced heuristics if needed
-    pass
